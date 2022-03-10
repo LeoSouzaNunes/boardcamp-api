ad33c1274d8d234604ad4ddd5fb42feab5e60b88
leo <celsolnv27@gmail.com>
2022-03-10 09:19:57 -0300

    Preparando revisão de código

--- src/schemas/rentalSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const rentalSchema = joi.object({
-    customerId: joi.number().integer().required(),
-    gameId: joi.number().integer().required(),
-    daysRented: joi.number().integer().greater(0).required(),
-});
-
-export default rentalSchema;
